cf4603600bb833e30a0872e8a43a40aebca8385e
Sultan <usembaevsultan08@gmail.com>
2020-08-11 21:03:19 +0600

    importing model from another app

--- news/apps/adminpanel/views.py
+++ news/apps/adminpanel/views.py
@@ -3,9 +3,9 @@ from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from django.utils import timezone
-
 from .models import Admin
-
+from django.apps import apps
+Article = apps.get_model('articles', 'Article')
 
 def index(request):
     return render(request, 'adminpanel/index.html')
@@ -18,14 +18,13 @@ def article_edit_page(request, art_id):
         art_id = int(art_id)
 
         try:
-            from news.news.apps.articles.models import Article
-            a = Article.objects.get(id=art_id)
-            print(a)
+            # a = Article.objects.get(id=art_id)
+            # print(a)
             article = {}
-            article["title"] = a.title
-            article["date"] = a.date
-            article["image_path"] = a.image_path
-            article["text"] = a.text.split('\n')
+            # article["title"] = a.title
+            # article["date"] = a.date
+            # article["image_path"] = a.image_path
+            # article["text"] = a.text.split('\n')
         except:
             raise Http404("Похоже такой статьи нет...")
         return render(request, 'adminpanel/article_edit_page.html', {"article": article})
@@ -36,6 +35,8 @@ def article_edit_page(request, art_id):
         image = request.POST["new_article_image"]
         text = request.POST["new_article_text"]
         print(title, image, date, text)
+        a = Article(title=title, date=date, text=text)
+        print(a)
         return HttpResponseRedirect( reverse("index") )
 
 
